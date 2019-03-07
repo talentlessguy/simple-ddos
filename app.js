bf439607b02327fac451ba320f3893ff80207f1a
talentless_guy <pilll.PL22@gmail.com>
2019-03-07 19:08:38 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+
+const request = require('request'), { log } = console
+
+module.exports = class {
+    constructor () {
+        this.errors = 0
+        this.success = 0
+    }
+    run(url = 'http://localhost', amount = 1000, interval = 1000) {
+        setInterval(() => {
+            for (let i = 0; i < amount; i++) {
+                request(url, err =>  err ? this.errors++ : this.success++)
+            }
+            log(`Errors: ${this.errors} Success: ${this.success}`)
+            this.errors = this.success = 0
+        }, interval)
+    }
+}
