1ea455fc991f1dfbb6d90348299ae2d72ea3ec88
v 1 r t l <35937217+talentlessguy@users.noreply.github.com>
2019-03-08 13:49:13 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const request = require('request'), { log } = console
 
 module.exports = (url = 'http://localhost', amount = 1000, interval = 1000) => {
-    this.errors = 0, this.success = 0
+    this.errors = this.success = 0
         setInterval(() => {
             for (let i = 0; i < amount; i++) {
                 request(url, err =>  err ? this.errors++ : this.success++)
